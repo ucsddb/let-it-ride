53bc7626ad34c4d4477687f5c06c6381c4d0ad1b
Daniel Xu <dxu@yahoo-inc.com>
2015-04-07 00:09:11 -0700

    add styling hack for homepage

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,17 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		init();
+
+		// TODO: figure out way to use container class for all views except home
+		// Ugly hack to remove container class to the view content for Home page image
+		$scope.$on('$destroy', function() {
+			angular.element(document.getElementsByClassName('main-container')).addClass('container');
+		});
+
+		function init() {
+			angular.element(document.getElementsByClassName('main-container')).removeClass('container');
+		}
 	}
 ]);
