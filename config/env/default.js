711a0cc5276b75a16f1fdf35c1b15eb411fcc56c
Daniel Xu <dxu429@gmail.com>
2015-09-28 00:45:02 -0700

    Update configs

--- config/env/default.js
+++ config/env/default.js
@@ -4,7 +4,7 @@ module.exports = {
     app: {
         title: 'Let It Ride',
         description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-        keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+        keywords: 'mongodb, express, angularjs, node.js, mongoose',
         googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
     },
     port: process.env.PORT || 3000,
