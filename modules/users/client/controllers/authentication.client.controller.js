ddaf811ab5e00f59e0bdc1fc6dd6700ac45a74b0
Daniel Xu <dxu@yahoo-inc.com>
2016-11-14 12:31:46 -0800

    Fix typeahead

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -5,13 +5,15 @@
         .module('users')
         .controller('AuthenticationController', AuthenticationController);
 
-    AuthenticationController.$inject = ['$scope', '$http', '$location', 'Authentication'];
+    AuthenticationController.$inject = ['$scope', '$http', '$location', 'Authentication', 'Locations'];
 
-    function AuthenticationController($scope, $http, $location, Authentication) {
+    function AuthenticationController($scope, $http, $location, Authentication, Locations) {
         $scope.authentication = Authentication;
 
         // If user is signed in then redirect back home
         if($scope.authentication.user) $location.path('/');
+
+        $scope.locations = Locations.query();
 
         $scope.signup = function() {
             $http.post('/api/auth/signup', $scope.credentials).success(function(response) {
